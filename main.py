1f4391c3b673cd50bb3baf73e1802fb95a139202
giacoballoccu <giacoballoccu@live.it>
2021-02-12 17:21:23 +0100

    Graphcomparison

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-from Trie import *
 import time
 
 def main():
-    t = Trie()
-    start = time.time()
-    t.populateTrieFromFile("Dataset/English-Words.txt")
-    end = time.time()
-    print("Time to build the trie: " + str(end - start) + "s")
-    start = time.time()
-    print(t.contains("maitlandite"))
-    end = time.time()
-    print("Time to lookup in the trie: " + str(end - start) + "s")
+    return 0
 if __name__ == "__main__":
     main()
