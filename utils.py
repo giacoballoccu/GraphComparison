8163d3746774f3e54d817ede6e41fc56c2c9c627
giacoballoccu <giacoballoccu@live.it>
2021-02-13 18:38:31 +0100

    Write to csv function util

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
 import csv
 
-def write_results_csv(path):
-    pass
+def write_results_csv(path, row):
+    with open(path, mode='a') as results:
+        writer = csv.writer(results)
+        writer.writerow(row)
+    results.close()
+
+def number_of_workers(self, sc):
+    return sc._jsc.sc().getExecutorMemoryStatus().size()
