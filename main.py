0ea55583ab48d916c7057f54a8e435f17e5b5eb7
giacoballoccu <giacoballoccu@live.it>
2021-02-13 13:11:26 +0100

    Main function

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import time
 
+import pyspark
+from pyspark.sql import SparkSession
+
+import StandardGraph
+import SparkGraph
+import GraphComparison
 def main():
-    return 0
+    conf = pyspark.SparkConf()
+    sc = pyspark.SparkContext(conf=conf)
+    ss = SparkSession(sc)
+    print(sc._conf.getAll())
+
+    standardGraph = StandardGraph("Dataset/twitch/ENGB/musae_ENGB_target.csv", "Dataset/twitch/ENGB/musae_ENGB_edges.csv")
+    sparkGraph =  SparkGraph("Dataset/twitch/ENGB/musae_ENGB_target.csv", "Dataset/twitch/ENGB/musae_ENGB_edges.csv")
+    compator = GraphComparison(sc, standardGraph, sparkGraph)
+
 if __name__ == "__main__":
     main()
