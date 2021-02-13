999cb935e3b638ca0fce18f4410cfd91324af0dd
giacoballoccu <giacoballoccu@live.it>
2021-02-13 13:11:10 +0100

    Moved GraphFrame in a class that allow easy initalization

---
+++ SparkGraph.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from graphframes import *
+
+
+class SparkGraph:
+    def __init__(self, spark_session, nodes_csv, edges_csv):
+        cols_names_edges = ['src', 'dst']
+        cols_names_nodes = ['long_id', 'days', 'mature', 'views', 'partner', 'id']
+        edges_df = pd.read_csv('/content/musae_ENGB_edges.csv', names=cols_names_edges, header=1)
+        nodes_df = pd.read_csv('/content/musae_ENGB_target.csv', names=cols_names_nodes, header=1)
+        edgesSparkDf = spark_session.createDataFrame(edges_df)
+        nodesSparkDf = spark_session.createDataFrame(nodes_df)
+        self._spark_graph = GraphFrame(nodesSparkDf, edgesSparkDf)
